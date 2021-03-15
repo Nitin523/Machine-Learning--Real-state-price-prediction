8275ef510a468c6f87d0b2fc3141434f7e420b36
Nitin Gangwar <54510381+Nitin523@users.noreply.github.com>
2021-03-15 09:15:17 +0530

    Update server.py

--- BHP/Server/server.py
+++ BHP/Server/server.py
@@ -24,7 +24,7 @@ def predict_home_price():
     })
     response.headers.add('Access-Control-Allow-Origin', '*')
 
-    return response
+    return response 
 
 if __name__ == "__main__":
     print("Starting Python Flask Server For Home Price Prediction...")
